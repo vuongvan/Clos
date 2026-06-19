68f7ff6a98d0bb36bd3dbc67884af6a35db2c283
Fei Yu <maivanvuong@gmail.com>
2026-06-20 00:00:34 +0700

    Initial commit

---
+++ KKPExProvider/build.gradle.kts
@@ -0,0 +1,30 @@
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+}
+
+// Use an integer for version numbers
+// Use an integer for version numbers
+version = 9
+
+cloudstream {
+    // All of these properties are optional, you can safely remove any of them.
+
+    description = "Watch content from Dailymotion"
+    authors = listOf("Luna712")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     */
+    status = 1 // Will be 3 if unspecified
+
+    tvTypes = listOf("Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.dailymotion.com&sz=%size%"
+
+    
+}
---
+++ OPExProvider/build.gradle.kts
@@ -0,0 +1,28 @@
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+}
+
+// Use an integer for version numbers
+version = 10
+
+cloudstream {
+    // All of these properties are optional, you can safely remove any of them.
+
+    description = "Watch content from Dailymotion"
+    authors = listOf("Luna712")
+
+    /**
+     * Status int as one of the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta-only
+     */
+    status = 1 // Will be 3 if unspecified
+
+    tvTypes = listOf("Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.dailymotion.com&sz=%size%"
+
+    
+}
